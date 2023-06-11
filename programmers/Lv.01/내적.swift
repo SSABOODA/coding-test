3b04563259e06f5a4e80472958a75c109155cf5a
SSABOODA <ssaboo92@gmail.com>
2023-06-11 16:43:53 +0900

    programmers-lv1-내적

---
+++ programmers/Lv.01/내적.swift
@@ -0,0 +1,8 @@
+// 내적
+// https://school.programmers.co.kr/learn/courses/30/lessons/70128
+
+import Foundation
+
+func solution(_ a:[Int], _ b:[Int]) -> Int {
+    return zip(a, b).map { $0 * $1 }.reduce(0, +)
+}
