a6b5a768982e2a8421a487c4ce86026be69c5e25
SSABOODA <ssaboo92@gmail.com>
2023-06-17 14:48:13 +0900

    programmers-lv2-신위 검색 수정

--- programmers/Lv.02/순위 검색.swift
+++ programmers/Lv.02/순위 검색.swift
@@ -3,53 +3,71 @@
 
 import Foundation
 
-func solution(_ info:[String], _ query:[String]) -> [Int] {
+func lowerbound(_ arr: [Int], _ score: Int) -> Int {
+    var low = 0
+    var high = arr.count
 
+    while low < high {
+        let mid = (low + high) / 2
+        if score <= arr[mid] {
+            high = mid
+        } else {
+            low = mid + 1
+        }
+    }
+    return arr.count - low
+}
+
+
+func solution(_ info:[String], _ query:[String]) -> [Int] {
     let infoArr:[[String]] = info.map { $0.components(separatedBy: " ") }
     let queryArr:[[String]] = query.map { $0.components(separatedBy: " and ")
                                          .joined(separator: " ")
                                          .components(separatedBy: " ")
-                                         .filter { $0 != "-" }
                                         }
 
-    var dict = Dictionary<String, [Int]>()
+    var infoDict = Dictionary<String, [Int]>()
 
     for info in infoArr {
-        var info = info
-        var infoScore = Int(info.removeLast())!
-        var infoStr = info.joined()
-        
-        if dict[infoStr] == nil {
-            dict[infoStr] = [infoScore]
-        } else {
-            dict[infoStr]?.append(infoScore)
-            dict[infoStr]?.sort(by: <)
+        let lang = [info[0], "-"]
+        let job = [info[1], "-"]
+        let rank = [info[2], "-"]
+        let food = [info[3], "-"]
+        let score = Int(info[4])!
+
+        for l in lang {
+            for j in job {
+                for r in rank {
+                    for f in food {
+                        let key = "\(l)\(j)\(r)\(f)"
+                        if infoDict[key] == nil {
+                            infoDict[key] = [score]
+                        } else {
+                            infoDict[key]?.append(score)
+                        }
+                    }
+                }
+            }
         }
     }
-    
-    let sortedDict = dict.sorted(by: { (a, b) in
-        return a.key < b.key
-    })
-    
-    var answer = [Int]()
-    
-    for query in queryArr {
 
-        var q = query
-        var score = Int(q.removeLast())!
-        var tempCount:Int = 0
-        for (key, value) in dict {
-            var filterResult = [String]()
-            for i in q {
-                if !key.contains(i) { break }
-                filterResult.append(i)
-            }
-            if filterResult.count == q.count {
-                let cnt = value.filter { $0 >= score }.count
-                tempCount += cnt
-            }
+    for (k, v) in infoDict {
+        let newVal = v.sorted(by: <)
+        infoDict[k] = newVal
+    }
+
+    var answer = [Int]()
+    for query in queryArr {
+        var query = query
+        let score = Int(query.removeLast())!
+        let compareQueryStr = query.joined()
+        let compareScore = infoDict[compareQueryStr]
+
+        if let scoreArr = compareScore {
+            answer.append(lowerbound(scoreArr, score))
+        } else {
+            answer.append(0)
         }
-        answer.append(tempCount)
     }
     return answer
 }
