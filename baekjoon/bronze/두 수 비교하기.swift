fb506d90f6b73b07304d3dcc38598cab90d58239
SSABOODA <ssaboo92@gmail.com>
2023-06-26 22:16:34 +0900

    baekjoon-bronze-두 수 비교하기

---
+++ baekjoon/bronze/두 수 비교하기.swift
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/1330
+
+var input = readLine()!.split(separator: " ").map { Int($0)! }
+
+if input[0] > input[1] {
+    print(">")
+} else if input[0] < input[1] {
+    print("<")
+} else {
+    print("==")
+}
+
