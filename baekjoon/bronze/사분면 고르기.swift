8aaaf1f4ff2ed1327f2d26ed9ef0768deb137105
SSABOODA <ssaboo92@gmail.com>
2023-06-26 22:18:30 +0900

    baekjoon-bronze-사분면 고르기

---
+++ baekjoon/bronze/사분면 고르기.swift
@@ -0,0 +1,14 @@
+// 사분면 고르기
+
+let x = Int(readLine()!)!
+let y = Int(readLine()!)!
+
+if x > 0 && y > 0 {
+    print(1)
+} else if x < 0 && y > 0 {
+    print(2)
+} else if x < 0 && y < 0 {
+    print(3)
+} else {
+    print(4)
+}
